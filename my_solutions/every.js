db630d6cef8e78ce076a453a928111627fd54377
Adam Hundley <adamhundley@gmail.com>
2016-05-14 22:24:04 -0600

    exercise 4

---
+++ my_solutions/every.js
@@ -0,0 +1,18 @@
+var _ = require("lodash");
+
+var theEvery = function (cities) {
+  var dividedCities = { hot: [], warm: []}
+
+  function checkTemps (temp) {
+
+  }
+
+  _.forEach(cities, function(cityName, temps))
+      if(){
+
+      }
+
+};
+
+
+module.exports = theEvery;
