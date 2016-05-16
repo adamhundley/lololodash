aae5293390c0f660a4ffe5224b926548c69aa48f
Adam Hundley <adamhundley@gmail.com>
2016-05-15 19:33:06 -0600

    exercise 7

---
+++ my_solutions/reduce.js
@@ -0,0 +1,20 @@
+var _ = require("lodash");
+
+var reducer = function(collection) {
+  var answer = []
+
+  var grouped = _.groupBy(collection, 'article');
+
+  _.forEach(grouped, function (all, name) {
+    var total = _.reduce(all, function (sum, order) {
+      return sum + order.quantity;
+    }, 0);
+
+    answer.push({ article: parseInt(name), total_orders: total });
+  });
+
+  return _.sortBy(answer, 'total_orders').reverse();
+  };
+
+
+module.exports = reducer;
